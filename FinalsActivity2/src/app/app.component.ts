fb170ae43b70f3b314962f35751b69744f4394d1
april-gomez <123357751+april-gomez@users.noreply.github.com>
2024-04-07 23:05:09 +0800

    Add files via upload

---
+++ FinalsActivity2/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+
+// AppComponent
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
+})
+export class AppComponent {
+  title = 'Finals Lab Activity 2';
+}
+
+//TabGroupBasicExample
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+export class TabGroupBasicExample {
+}
